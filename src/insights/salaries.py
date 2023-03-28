293ccc750aa964f374f6941410802507090ad41f
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-03-27 22:48:46 -0300

    feat: adiciona funcao para verificar salario

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -48,6 +48,25 @@ def get_min_salary(path: str) -> int:
     return min(all_min_salaries)
 
 
+def convert_salary_to_int(
+    min_salary_key: Union[int, str], max_salary_key: Union[int, str]
+) -> tuple:
+    if isinstance(min_salary_key, int) and isinstance(max_salary_key, int):
+        return (min_salary_key, max_salary_key)
+    min_salary_is_valid = (
+        isinstance(min_salary_key, str) and min_salary_key.isdigit()
+    )
+    max_salary_is_valid = (
+        isinstance(max_salary_key, str) and max_salary_key.isdigit()
+    )
+    if min_salary_is_valid and max_salary_is_valid:
+        return (int(min_salary_key), int(max_salary_key))
+    else:
+        raise ValueError(
+            "'min_salary' and 'max_salary' values must be valid integers."
+        )
+
+
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
     """Checks if a given salary is in the salary range of a given job
 
@@ -71,7 +90,25 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    raise NotImplementedError
+    int_salary = salary
+    # verifica type de salário
+    if isinstance(int_salary, str) and int_salary.isdigit():
+        int_salary = int(int_salary)
+    if not (isinstance(int_salary, int)):
+        raise ValueError("'salary' isn't a valid integer")
+    # verifica se as chaves "min_salary" e "max_salary existem"
+    if not ("min_salary" in job and "max_salary" in job):
+        raise ValueError("'max_salary' and 'min_salary' keys are required.")
+    # converte os valores da chave se for necessário e possível
+    (min_salary, max_salary) = convert_salary_to_int(
+        job["min_salary"], job["max_salary"]
+    )
+    if min_salary > max_salary:
+        raise ValueError(
+            "'max_salary' value must be greater than 'min_salary' value."
+        )
+    it_matches = min_salary <= int_salary <= max_salary
+    return it_matches
 
 
 def filter_by_salary_range(
