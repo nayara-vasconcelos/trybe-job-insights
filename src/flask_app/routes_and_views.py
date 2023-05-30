61bec2bb7397eb3e87322df2dcd16006d752183c
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-30 18:59:56 -0300

    feat: implementa a pagina de um job com Flask

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -17,6 +17,7 @@ from src.insights.salaries import (
 )
 
 from src.flask_app.more_insights import (
+    get_job,
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
@@ -77,6 +78,14 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs = read("data/jobs.csv")
+    job_info = get_job(jobs, index)
+
+    return render_template("job.jinja2", job=job_info)
 
 
 def init_app(app: Flask):
