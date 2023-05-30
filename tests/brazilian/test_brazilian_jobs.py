77aa2ab6be77b18427ee74c1f064ef99cec670e3
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-29 21:38:12 -0300

    test: testa a funcao read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,22 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from unittest.mock import mock_open, patch
 
 
 def test_brazilian_jobs():
-    pass
+    mock_data = (
+        "titulo,salario,tipo\n"
+        + "Maquinista,2000,trainee\n"
+        + "Motorista,3000,full time"
+    )
+
+    expected_output = [
+        {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+        {"title": "Motorista", "salary": "3000", "type": "full time"},
+    ]
+
+    with pytest.raises(ValueError, match="Invalid file format"):
+        read_brazilian_file("any_file_path.json")
+
+    with patch("builtins.open", mock_open(read_data=mock_data)):
+        assert read_brazilian_file("any_file_path.csv") == expected_output
